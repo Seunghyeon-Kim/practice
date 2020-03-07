cb3ded4415400b4098efc69e8621a62129807de9
Seunghyeon-Kim <ssseungzz7@gmail.com>
2020-03-08 00:07:31 +0900

    src 등록

---
+++ src/main/java/example/org/practice/service/posts/PostsService.java
@@ -0,0 +1,41 @@
+package example.org.practice.service.posts;
+
+import example.org.practice.domain.posts.Posts;
+import example.org.practice.domain.posts.PostsRepository;
+import example.org.practice.web.dto.PostsResponseDto;
+import example.org.practice.web.dto.PostsSaveRequestDto;
+import example.org.practice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+            IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
