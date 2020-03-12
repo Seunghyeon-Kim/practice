11b696b5eeefbf45b38268b6385b96272f14d1b4
Seunghyeon-Kim <ssseungzz7@gmail.com>
2020-03-12 17:43:21 +0900

    @EnableJapAuditing과 @SpringBootApplication 분리 - Application에서 @EnableJpaAuditing 제거 - config 패키지에 JpaConfig 생성

---
+++ src/main/java/example/org/practice/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package example.org.practice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
